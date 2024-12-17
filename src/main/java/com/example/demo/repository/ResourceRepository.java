293574d5ff9b9dee1c81e84c7398d39111915203
torrezhan <torezanzoldasbek@gmail.com>
2024-12-18 00:34:53 +0500

    put and delete

--- src/main/java/com/example/demo/repository/ResourceRepository.java
+++ src/main/java/com/example/demo/repository/ResourceRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 }
