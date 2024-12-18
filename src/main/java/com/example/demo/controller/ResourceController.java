0d0089a3de4ee1a108502b1e1c1d92aec6ea71eb
torrezhan <torezanzoldasbek@gmail.com>
2024-12-19 04:54:34 +0500

    commit

--- src/main/java/com/example/demo/controller/ResourceController.java
+++ src/main/java/com/example/demo/controller/ResourceController.java
@@ -14,25 +14,21 @@ public class ResourceController {
     @Autowired
     private ResourceService resourceService;
 
-    // POST: Создание нового ресурса
     @PostMapping
     public Resource createResource(@RequestBody Resource resource) {
         return resourceService.saveResource(resource);
     }
 
-    // GET: Получение списка всех ресурсов
     @GetMapping
     public List<Resource> getAllResources() {
         return resourceService.getAllResources();
     }
 
-    // PUT: Обновление существующего ресурса
     @PutMapping("/{id}")
     public Resource updateResource(@PathVariable Long id, @RequestBody Resource resource) {
         return resourceService.updateResource(id, resource);
     }
 
-    // DELETE: Удаление ресурса по id
     @DeleteMapping("/{id}")
     public void deleteResource(@PathVariable Long id) {
         resourceService.deleteResource(id);
--- src/main/java/com/example/demo/service/ResourceService.java
+++ src/main/java/com/example/demo/service/ResourceService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ResourceService {
@@ -14,27 +13,23 @@ public class ResourceService {
     @Autowired
     private ResourceRepository resourceRepository;
 
-    // Сохранение нового ресурса
     public Resource saveResource(Resource resource) {
         return resourceRepository.save(resource);
     }
 
-    // Получение всех ресурсов
     public List<Resource> getAllResources() {
         return resourceRepository.findAll();
     }
 
-    // Обновление существующего ресурса
     public Resource updateResource(Long id, Resource resource) {
         if (resourceRepository.existsById(id)) {
-            resource.setId(id); // Устанавливаем id ресурса для обновления
+            resource.setId(id);
             return resourceRepository.save(resource);
         } else {
             throw new RuntimeException("Resource not found");
         }
     }
 
-    // Удаление ресурса
     public void deleteResource(Long id) {
         if (resourceRepository.existsById(id)) {
             resourceRepository.deleteById(id);
